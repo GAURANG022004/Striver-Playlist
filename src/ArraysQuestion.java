dac1e32e40f24127b7f7cf90ef0bbae3b0e930d8
Gaurang <gaurangmali11@gmail.com>
2026-07-25 21:36:19 +0530

    Update Code

--- src/ArraysQuestion.java
+++ src/ArraysQuestion.java
@@ -250,18 +250,17 @@ public class ArraysQuestion
         return sum2 - sum;
     }
 
-    public static int countConsecutiveTerm(int arr[]){
+    public static int countConsecutiveTerm(int arr[]) {
         int n = arr.length;
         int count = 0;
         int maxCount = 0;
         // 1, 1, 0, 1, 1, 1
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
 
-            if(arr[i] == 1){
+            if (arr[i] == 1) {
                 count++;
-            }
-            else{
+            } else {
                 count = 0;
             }
 
@@ -269,7 +268,6 @@ public class ArraysQuestion
         }
 
         return maxCount;
-
 
     }
 
